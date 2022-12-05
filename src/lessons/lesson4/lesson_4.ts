7bdfd503672abaf3ca3ea87a53391a312129ecc3
Natalia Kondratieva <kondratieva07@gmail.com>
2022-12-05 21:25:28 +0300

    lesson 4 done

--- src/lessons/lesson4/lesson_4.ts
+++ src/lessons/lesson4/lesson_4.ts
@@ -7,19 +7,34 @@
 // Task 01
 // Создайте промис, который постоянно находиться в состоянии pending.
 // В конструкторе промиса выведите в консоль сообщение "Promise is created".
+const promise01 = new Promise((resolve, reject) => {
+    console.log('Promise is created')
+})
 
 
 // Task 02
 // Создайте промис, который после создания сразу же переходит в состояние resolve
 // и возвращает строку 'Promise Data'
 // Получите данные промиса и выведите их в консоль
-//
+
+const promise02 = new Promise((resolve, reject) => {
+    resolve('Promise Data')
+}).then((data) => {
+    console.log(data)
+})
 
 // Task 03
 // Создайте промис, который после создания сразу же переходит в состояние rejected
 // и возвращает строку 'Promise Error'
 // Получите данные промиса и выведите их в консоль
 
+const promise03 = new Promise((resolve, reject) => {
+    reject('Promise Error')
+}).catch((data) => {
+    console.log(data)
+})
+
+const promise031 = Promise.reject('Error').catch((data) => console.log(data))
 
 // Task 04
 // Создайте промис, который переходит в состояние resolved через 3с.
@@ -27,6 +42,11 @@
 // и возвращает строку 'Promise Data'
 // Получите данные промиса и выведите их в консоль
 
+const promise04 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Promise Data')
+    }, 3000)
+}).then((data) => console.log(data))
 
 // Task 05
 // Создайте литерал объекта handlePromise со следующими свойствами:
@@ -40,7 +60,20 @@
 // описаного выше объекта: свойство promise получает новый созданный промис,
 // свойства resolve и reject получают ссылки на соответствующие функции
 // resolve и reject. Следующие два обработчика запускают методы resolve и reject.
-
+const handlePromise = {
+    promise: null,
+    resolve: null,
+    reject: null,
+    onSuccess: function (paramName: string) {
+        console.log(`Promise is resolved with data: ${paramName}`)
+    },
+    onError: function (paramName: string) {
+        console.log(`Promise is rejected with error: ${paramName}`)
+    }
+}
+// let createPromise = document.getElementById('btn-create-promise')
+// const resolvePromise = document.getElementById('btn-resolve-promise')
+// const rejectPromise = document.getElementById('btn-reject-promise')
 
 // Task 06
 // Создайте промис, который через 1 с возвращает строку "My name is".
@@ -49,14 +82,58 @@
 // Создайте функцию print, которая выводит в консоль значение своего параметра
 // Добавьте два метода then и передайте созданные функции.
 
+const promise06 = new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+        resolve('My name is ')
+    }, 1000)
+}).then((data) => print(onSuccess(data)))
+
+const onSuccess = (data: string) => {
+    return data + 'Natalia'
+}
+const print = (param:string) => {
+    console.log(param)
+}
 
 // Task 7
 // Создайте три промиса. Первый промис возвращает объект { name: "Anna" } через 2с,
 // второй промис возвращает объект {age: 16} через 3 с, а третий {city: ''} через 4с.
 // Получите результаты работы промисов, объедините свойства объектов
 // и выведите в консоль {name, age, city}
-
+const promise071 = new Promise<{name:string}>((resolve, reject) => {
+    setTimeout(() => {
+        resolve({ name: "Anna" })
+    }, 2000)
+})
+const promise072 = new Promise<{age:number}>((resolve, reject) => {
+    setTimeout(() => {
+        resolve( {age: 16})
+    }, 3000)
+})
+const promise073 = new Promise<{city:string}>((resolve, reject) => {
+    setTimeout(() => {
+        resolve({city: ''})
+    }, 4000)
+})
+Promise.all([promise071, promise072, promise073]).then(date=> {
+    console.log(date)
+})
+let name: string
+promise071.then((data) => {
+    return name = data.name
+})
+let age: number
+promise072.then((data) => {
+    return age = data.age
+})
+let city: string
+promise073.then((data) => {
+    return city = data.city
+})
+    .then(() => {
+        console.log({name, age, city})
+    })
 
 // just a plug
- export default () => {
- };
+export default () => {
+};
